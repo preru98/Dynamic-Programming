1a2edf3f9965d51e7c19c2fe1b05df1661a6b15f
Prerna <prerna1998.ps@gmail.com>
2020-03-26 07:04:34 +0530

    Minimal coins for change | Dynamic Programming

---
+++ minChange/minChangeDynamicProgramming.cpp
@@ -0,0 +1,49 @@
+/*
+
+Problem  : Given an amount, find the minimum number of coins required to make the exact amount of change. 
+Solution : Dynamic Programming
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int min_coins(int, int[],int);
+int min_coins(int, int[],int,int[]);
+int main(){
+    int change=12;
+    int coins[]={1,2,5,10};
+    int size=sizeof(coins)/sizeof(coins[0]);
+    cout<<min_coins(change,coins,size);
+}
+
+int min_coins(int change, int coins[], int size){
+    int cache[50];
+    for(int i=0;i<change;i++){
+        cache[i]=-1;
+    }
+    return min_coins(change,coins,size,cache);
+}
+
+int min_coins(int change, int coins[],int size, int cache[]){
+    if(change==0){
+        return 0;
+    }
+    int min=change;
+    for(int i=0;i<size;i++){
+        if(change-coins[i]>=0){
+            int count;
+            if(cache[change-coins[i]]>=0){
+                count=cache[change-coins[i]];
+            }
+            else{
+                count=min_coins(change-coins[i],coins,size,cache);
+                cache[change-coins[i]]=count;
+            }
+            if(count+1<min){
+                min=count+1;
+            }
+        }
+    }
+    return min;
+}
